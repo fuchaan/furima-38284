4a74c2a3358c2d9e21a68d7a4817aa5c3c3395d4
fuchaan <miyamoto.fuma1@gmail.com>
2023-02-21 21:14:51 +0900

    ユーザー登録エラーメッセージの日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,16 @@ class User < ApplicationRecord
   validates :first_name,      presence: true
   validates :last_name,       presence: true
   validates :first_name, :last_name,
-            format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/, message: 'is invalid. Input full-width characters' }
+            format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/, message: 'が無効です。全角文字で入力してください' }
   validates :first_name_kana, presence: true
   validates :last_name_kana,  presence: true
   validates :first_name_kana, :last_name_kana,
-            format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters' }
+            format: { with: /\A[ァ-ヶー－]+\z/, message: 'が無効です。全角カタカナで入力してください' }
   validates :nickname,        presence: true
   validates :birthday,        presence: true
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers' }
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'が無効です。文字と数字の両方を含めて入力してください' }
 
   has_many :items
   has_many :buys
